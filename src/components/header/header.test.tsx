a89f73166b71972afe7fdc923f26d5d4a9d38fdf
FrozenWellStudios <richardcoldwell74@gmail.com>
2022-07-24 22:01:49 +0100

    mock session in header test

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,10 +1,20 @@
 import { render } from "@testing-library/react";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
 import Header from "./header";
 
 describe("Header", () => {
   it("should render successfully", () => {
-    const { baseElement } = render(<Header />);
+    const mockSession: Session = {
+      expires: "1",
+      user: { email: "a", name: "Delta", image: "c" },
+    };
+    const { baseElement } = render(
+      <SessionProvider session={mockSession}>
+        <Header />
+      </SessionProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
